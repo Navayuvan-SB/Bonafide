c7e873a2561f17f8c2116f0341c2efb2b03957e2
navayuvan-sb <ns.navayuvan@gmail.com>
2019-08-13 17:38:39 +0530

    Structured

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
+
+firebase.auth().onAuthStateChange(firebaseUser =>{
+	if(firebaseUser){
+		window.alert(firebaseUser);
+	}else{
+		window.alert('not logged in');
+	}
+});
+
 function login(){
 
 	var userEmail = document.getElementById('Email_field').value;
@@ -6,14 +15,8 @@ function login(){
 	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
-	});
+	  window.alert("Error in Login");
 
-	firebase.auth().onAuthStateChange(firebaseUser =>{
-		if(firebaseUser){
-			console.log(firebaseUser);
-		}else{
-			console.log('not logged in');
-		}
 	});
 
 }
--- php/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define('DB_NAME', 'bonafide');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'toorgmai');
-	define("DB_HOST", 'localhost');
